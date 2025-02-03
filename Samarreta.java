f4aeb12762f205d5c20c2eb2168f2e522bfc1868
Marco Poelsma <marco.poelsma@gracia.lasalle.cat>
2025-02-03 09:26:24 +0100

    Afegir info a mètodes per a que no hi hagi variables sense usar

--- Samarreta.java
+++ Samarreta.java
@@ -1,14 +1,26 @@
 public class Samarreta {
     String color;
     byte talla; // Va de 0 (XS) a 5 (XXL)
-    static boolean disponible = true;
+    private static boolean disponible = true;
 
     public void provar() {
-        System.out.println("T'estàs provant una samarreta");
+        System.out.println("T'estàs provant una samarreta de color " + color + " i talla " + Samarreta.getTalla(talla));
         if (disponible) {
             System.out.println("Et queda genial");
         } else {
             System.out.println("No hi ha cap samarreta disponible");
+        }
+    }
+
+    public static String getTalla(byte talla) {
+        switch (talla) {
+            case 0 -> {return "XS";}
+            case 1 -> {return "S";}
+            case 2 -> {return "M";}
+            case 3 -> {return "L";}
+            case 4 -> {return "XL";}
+            case 5 -> {return "XXL";}
+            default -> {return "Talla no vàlida";}  
         }
     }
 
